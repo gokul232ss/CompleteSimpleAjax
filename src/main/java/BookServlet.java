8309169b94b2a576f938ab47dd99dab38918b59f
Trainee <Trainee@KGFSL.COM>
2018-02-13 18:50:13 +0530

    first commit

---
+++ src/main/java/BookServlet.java
@@ -0,0 +1,104 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
+@WebServlet("/")
+public class BookServlet extends HttpServlet {
+@Override
+protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    doPost(req, resp);
+}
+@Override
+protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+   String action=req.getServletPath();
+   //PrintWriter out=resp.getWriter();
+   try{
+   switch(action){
+       case "/insert":
+     //           out.println("case 1");
+                
+                insertbook(req,resp);
+                 break;
+       case "/edit":
+       
+               updatebook(req,resp);
+               break;
+        case "/update":
+        
+                updatefeild(req,resp);
+                break;
+        case "/delete":
+                 deleteFeild(req,resp);
+                 break;
+        default:
+       
+                listbook(req,resp);
+                break;
+
+   }
+}
+catch(SQLException e){
+    e.printStackTrace();
+}
+}
+private void deleteFeild(HttpServletRequest req, HttpServletResponse resp) throws IOException, SQLException {
+    new BookDAO().deletBook(new Book(Integer.parseInt(req.getParameter("code"))));
+    resp.sendRedirect("/");
+}
+private void updatefeild(HttpServletRequest req, HttpServletResponse resp) throws IOException, SQLException {
+    int id=Integer.parseInt(req.getParameter("id"));
+    String title=req.getParameter("title");
+    String author=req.getParameter("author");
+    Float price=Float.parseFloat(req.getParameter("price"));
+    Book book=new Book(id,title,author,price);
+    BookDAO bookDAO=new BookDAO();
+    bookDAO.updateFeild(book);
+    resp.sendRedirect("/");
+}
+private void updatebook(HttpServletRequest req, HttpServletResponse resp) throws SQLException, IOException {
+    Book book=new Book(Integer.parseInt(req.getParameter("id")));
+   BookDAO bookDAO=new BookDAO();
+   Book refbook=bookDAO.updatebook(book);
+   List<Book> al=new ArrayList<Book>();
+   al.add(refbook);
+   Gson  gson=new Gson();
+   JsonElement ele=gson.toJsonTree(al,new TypeToken<List<Book>>(){}.getType());
+   JsonArray j=ele.getAsJsonArray();
+   resp.setContentType("application/json");
+ 
+   resp.getWriter().print(j);
+
+
+}
+private void insertbook(HttpServletRequest req, HttpServletResponse resp) throws SQLException, IOException {
+    String title=req.getParameter("title");
+    String author=req.getParameter("author");
+    Float price=Float.parseFloat(req.getParameter("price"));
+    Book book=new Book(title,author,price);
+    BookDAO bookDAO=new BookDAO();
+    bookDAO.insert(book);
+    resp.sendRedirect("/");
+
+}
+private void listbook(HttpServletRequest req, HttpServletResponse resp) throws SQLException, IOException {
+    BookDAO bookdao=new BookDAO();
+    List<Book> listBook=bookdao.listbookdao();
+    Gson gson=new Gson();
+    JsonElement element=gson.toJsonTree(listBook,new TypeToken<List<Book>>() {}.getType());
+    JsonArray jsonArray=element.getAsJsonArray();
+    resp.setContentType("application/json");
+    
+    resp.getWriter().print(jsonArray);
+}
+}
